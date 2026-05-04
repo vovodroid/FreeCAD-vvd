0b6f4b3ce4e868b2955f46a46d70a429391a77d3
vovodroid <vovodroid@users.noreply.github.com>
2026-05-04 09:43:46 +0300

    Edit sketch: draw sketch on top.

--- src/Mod/Sketcher/Gui/ViewProviderSketch.h
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.h
@@ -50,6 +50,7 @@
 #include "ShortcutListener.h"
 #include "Utils.h"
 
+#include <Gui/Inventor/So3DAnnotation.h>
 #include <Gui/Inventor/SoToggleSwitch.h>
 #include <Mod/Part/Gui/ViewProviderPreviewExtension.h>
 
@@ -1039,6 +1040,7 @@ private:
     std::string editObjName;
     std::string editSubName;
 
+    Gui::So3DAnnotation* pcGridAnnotation = nullptr;
     Gui::CoinPtr<SoSketchFaces> pcSketchFaces;
     Gui::CoinPtr<SoToggleSwitch> pcSketchFacesToggle;
 
