8c5ba443757091caa8f5f4c383d0fff33e22f364
vovodroid <vovodroid@users.noreply.github.com>
2026-05-10 11:19:34 +0300

    View mode: draw sketch on top

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -689,6 +689,9 @@ ViewProviderSketch::~ViewProviderSketch()
     if (pcGridAnnotation) {
         pcGridAnnotation->unref();
     }
+    if (pcModeSwitchAnnotation) {
+        pcModeSwitchAnnotation->unref();
+    }
 }
 
 void ViewProviderSketch::slotUndoDocument(const Gui::Document& /*doc*/)
@@ -3844,7 +3847,16 @@ bool ViewProviderSketch::getDetailPath(
         }
     }
 
-    return ViewProvider2DObject::getDetailPath(subname, pPath, append, det);
+    if (ViewProvider2DObject::getDetailPath(subname, pPath, append, det)) {
+        return true;
+    }
+    // pcModeSwitch was wrapped in SoAnnotation
+    if (pcModeSwitchAnnotation) {
+        pPath->append(pcRoot);
+        pPath->append(pcModeSwitch);
+        return true;
+    }
+    return false;
 }
 // clang-format off
 
@@ -3852,7 +3864,13 @@ void ViewProviderSketch::attach(App::DocumentObject* pcFeat)
 {
     ViewProvider2DObject::attach(pcFeat);
 
-    getOrCreateAnnotation()->addChild(pcSketchFacesToggle);
+    // Extract the mode switch from root and wrap it in a SoAnnotation to draw on top
+    pcModeSwitchAnnotation = new SoAnnotation;
+    pcModeSwitchAnnotation->ref();
+    pcModeSwitchAnnotation->setName("SketchModeSwitchAnnotation");
+    pcModeSwitchAnnotation->addChild(pcModeSwitch);
+    pcRoot->removeChild(pcModeSwitch);
+    pcRoot->addChild(pcModeSwitchAnnotation);
 }
 
 void ViewProviderSketch::setupContextMenu(QMenu* menu, QObject* receiver, const char* member)
